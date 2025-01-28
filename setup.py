e2693e151559bc0863f09f26a7802063c974ba9a
codingJang <56782623+codingJang@users.noreply.github.com>
2025-01-28 12:06:23 +0000

    tryng  gain

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(name='symmetrizer',
       version='0.1',
       description='Symmetrizer package, from the paper "MDP homomorphic Networks: Group Symmetries in Reinforcement Learning"',
       author='Elise van der Pol, Daniel Worrall',
-      packages=find_packages(include=['symmetrizer']),
+      package_dir={"", "symmetrizer"}
       url='https://github.com/ElisevanderPol/symmetrizer',
       license='MIT License',
       zip_safe=False)
+      
